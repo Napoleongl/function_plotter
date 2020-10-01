de6d987664cdb6f17537e8fa09477543e3164754
Napoleongl <guslu389@student.liu.se>
2020-10-01 10:57:15 +0200

    added helper to remove empty inputs

--- helpers.R
+++ helpers.R
@@ -8,6 +8,10 @@ correct_decimal_comma <- function(function_strings){
   gsub(pattern = "\\,", replacement = "\\.", x = function_strings)
 }
 
+remove_empty <- function(function_strings){
+  function_strings[function_strings != "" & !is.null(function_strings) & !is.na(function_strings)]
+}
+
 evaluate_functions <- function(function_strings, x){
   # Evaluates a vector of function strings over an input space x
   as.data.table(cbind(x, sapply(function_strings, function(func){eval(parse(text = func))})))
